6c09bbba09fae74fc8a98bd3e956a9db1ac9ca8d
SWang848 <swang848@uw.edu>
2021-10-19 00:47:58 +0800

    gpu setting

--- agent.py
+++ agent.py
@@ -28,8 +28,8 @@ from utils import *
 
 from pprint import pprint
 
-# physical_devices = tf.config.experimental.list_physical_devices('GPU')
-# tf.config.experimental.set_memory_growth(physical_devices[0], True)
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 def LEAKY_RELU(): return LeakyReLU(0.01)
 
